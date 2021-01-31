7ad2d2b7dea07689614a58bc84eab22da5dc3a22
PREMINDA <premindabandara0777@gmail.com>
2021-02-01 00:24:31 +0530

    Player Jump Fix

--- Assets/Assets/Script/Player.cs
+++ Assets/Assets/Script/Player.cs
@@ -7,12 +7,15 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rg;
     [SerializeField]
     private float _jumpheight=5.0f;
-    private bool _isground = true;
-    private bool _re_jump = false;
+    [SerializeField]
+    private float _Speed = 2.5f;
+    private PlayerAnimation _Movedirection;
+    
     // Start is called before the first frame update
     void Start()
     {
         _rg = gameObject.GetComponent<Rigidbody2D>();
+        _Movedirection = transform.GetComponent<PlayerAnimation>();
        
 
     }
@@ -20,33 +23,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float Hmove = Input.GetAxisRaw("Horizontal");
-        float Vmove = Input.GetAxisRaw("Vertical");
-        if(Input.GetKeyDown(KeyCode.Space) && _isground == true)
-        {
-            _rg.velocity = new Vector2(_rg.velocity.x, _jumpheight);
-            _isground = false;
-            _re_jump = true;
-            StartCoroutine(wait());
-        }
-
-        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position,Vector2.down,1.0f,1<<8);
-        if(hitinfo.collider != null)
-        {
-            if(_re_jump == false) { 
-            _isground = true;
-            }
-        }
-            _rg.velocity = new Vector2(Hmove, _rg.velocity.y);
+        Movement();
+        
+        
         
         
     }
-    private IEnumerator wait()
+    
+    private void Movement() {
+
+        float Hmove = Input.GetAxisRaw("Horizontal");
+        float Vmove = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space) && Isground() == true)
+        {
+            _rg.velocity = new Vector2(_rg.velocity.x, _jumpheight);
+            
+           
+        }
+        
+        _rg.velocity = new Vector2(Hmove*_Speed, _rg.velocity.y);
+        _Movedirection.Move(Hmove);
+
+    }
+   
+    private bool Isground()
     {
-        
-        yield return new WaitForSeconds(0.1f);
-        _re_jump = false;
-        
+
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
+        if (hitinfo.collider != null)
+        {
+            return true;
+        }
+        return false;
 
     }
 }
---
+++ Assets/Assets/Script/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+}
