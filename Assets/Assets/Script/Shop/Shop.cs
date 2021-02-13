6b245a9de82533959d164a0ee965ea20d995594e
PREMINDA <premindabandara0777@gmail.com>
2021-02-13 23:42:31 +0530

    Shop Enable

---
+++ Assets/Assets/Script/Shop/Shop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanal;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                shopPanal.SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                shopPanal.SetActive(false);
+            }
+
+
+        }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            shopPanal.SetActive(false);
+        }
+    }
+}
